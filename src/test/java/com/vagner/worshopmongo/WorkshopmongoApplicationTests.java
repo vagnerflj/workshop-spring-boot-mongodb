d600935f2b704206d8131e9872608f66c4e310ef
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-23 11:05:19 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/vagner/worshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagner.worshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

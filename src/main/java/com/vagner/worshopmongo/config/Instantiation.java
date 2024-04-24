996eca4a1e4ea36b6d5892740a7e9b5f8d6efca4
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-24 10:42:31 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/vagner/worshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.vagner.worshopmongo.config;
+
+import com.vagner.worshopmongo.domain.User;
+import com.vagner.worshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}

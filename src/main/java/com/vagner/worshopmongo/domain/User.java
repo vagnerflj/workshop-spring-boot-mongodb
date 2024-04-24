89ec3d757f3ad2dc51a6845dbc2bbf7ed26167d5
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-24 10:27:40 -0300

    Conectando ao MongoDB com repositoy e service

--- src/main/java/com/vagner/worshopmongo/domain/User.java
+++ src/main/java/com/vagner/worshopmongo/domain/User.java
@@ -1,18 +1,24 @@
 package com.vagner.worshopmongo.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
 
-    public User(){
+    public User() {
     }
 
     public User(String id, String name, String email) {
+        super();
         this.id = id;
         this.name = name;
         this.email = email;
@@ -43,17 +49,27 @@ public class User implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        return Objects.equals(id, user.id);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
---
+++ src/main/java/com/vagner/worshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.vagner.worshopmongo.repository;
+
+import com.vagner.worshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}

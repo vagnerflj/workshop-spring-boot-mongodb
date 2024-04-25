6a94f10ad56470332896cb49c53d9a5e41601b10
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-25 09:56:16 -0300

    Obtendo um usuario por id

--- src/main/java/com/vagner/worshopmongo/resources/UserResource.java
+++ src/main/java/com/vagner/worshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.vagner.worshopmongo.dto.UserDTO;
 import com.vagner.worshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+    @RequestMapping(value="/{id}",method=RequestMethod.GET)
+    public ResponseEntity<UserDTO> finById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }

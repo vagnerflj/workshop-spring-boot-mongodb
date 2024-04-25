79fa4dcae4d604cd2392bc128e649fd6b68f68e5
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-25 10:41:52 -0300

    utualização de usuario com PUT

--- src/main/java/com/vagner/worshopmongo/resources/UserResource.java
+++ src/main/java/com/vagner/worshopmongo/resources/UserResource.java
@@ -30,7 +30,7 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
-    @PostMapping
+    @RequestMapping(method=RequestMethod.POST)
     public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
         User obj = service.fromDTO(objDto);
         obj = service.insert(obj);
@@ -40,6 +40,13 @@ public class UserResource {
     @RequestMapping(value="/{id}",method=RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable String id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 }

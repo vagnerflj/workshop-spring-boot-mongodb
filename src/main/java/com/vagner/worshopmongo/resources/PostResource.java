80d49b50912dc0e6895108ad40e65e2b03196cce
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-26 10:47:34 -0300

    Consulta simples com query methods

--- src/main/java/com/vagner/worshopmongo/resources/PostResource.java
+++ src/main/java/com/vagner/worshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.vagner.worshopmongo.resources;
 import com.vagner.worshopmongo.domain.Post;
 import com.vagner.worshopmongo.domain.User;
 import com.vagner.worshopmongo.dto.UserDTO;
+import com.vagner.worshopmongo.resources.util.URL;
 import com.vagner.worshopmongo.services.PostService;
 import com.vagner.worshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,11 @@ public class PostResource {
     public ResponseEntity<Post> finById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> finByTitle(@RequestParam(value="text", defaultValue= "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/vagner/worshopmongo/services/PostService.java
+++ src/main/java/com/vagner/worshopmongo/services/PostService.java
@@ -22,5 +22,9 @@ public class PostService {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
 

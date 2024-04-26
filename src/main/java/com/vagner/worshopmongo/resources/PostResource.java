06a9312457c5ab253a3160c4e8f845eb2a67efbe
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-26 10:11:52 -0300

    Obtendo post por id

---
+++ src/main/java/com/vagner/worshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.vagner.worshopmongo.resources;
+
+import com.vagner.worshopmongo.domain.Post;
+import com.vagner.worshopmongo.domain.User;
+import com.vagner.worshopmongo.dto.UserDTO;
+import com.vagner.worshopmongo.services.PostService;
+import com.vagner.worshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> finById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/vagner/worshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.vagner.worshopmongo.services;
+
+import com.vagner.worshopmongo.domain.Post;
+import com.vagner.worshopmongo.domain.User;
+import com.vagner.worshopmongo.dto.UserDTO;
+import com.vagner.worshopmongo.repository.PostRepository;
+import com.vagner.worshopmongo.repository.UserRepository;
+import com.vagner.worshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
+
